6a7760f85657236c4e4da94bc758b162d23a64a1
Getonto3 <getontomoses18@gmail.com>
2024-02-14 08:45:35 +0300

    feat: custom button

---
+++ lib/views/customButton.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:public_repo/views/customtext.dart';
+import 'package:public_repo/configs/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final Function onPressed;
+  final Color buttonColor;
+  final double width;
+
+  const CustomButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.buttonColor = appGrayColor,
+    this.width = 18,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: CustomText(
+        label: label,
+        labelcolor: Colors.white,
+      ),
+      style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor, minimumSize: Size(width, 50)),
+    );
+  }
+}

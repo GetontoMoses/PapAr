765f8851cfa34f62d606eb855df1e764f28d7caa
Getonto3 <getontomoses18@gmail.com>
2024-04-12 02:57:29 +0300

    feat: handling pdf preview in downloads

---
+++ lib/views/filepreview.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class PdfPreviewPage extends StatelessWidget {
+  final List<File> pdfFiles;
+  final int initialIndex;
+
+  PdfPreviewPage({Key? key, required this.pdfFiles, this.initialIndex = 0})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final File pdfFile = pdfFiles[initialIndex];
+    print('PDF File Path: ${pdfFile.path}'); // Debugging output
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: SfPdfViewer.file(pdfFile),
+      ),
+    );
+  }
+}
